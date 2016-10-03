3b436302e1afdf812d94ecedb6f651a780267430
sehgalayush1 <sehgalayush26@gmail.com>
2016-10-03 12:04:09 +0530

    edited youtube script

--- Youtube.py
+++ Youtube.py
@@ -1,17 +1,17 @@
 import pafy
 
-url= raw_input("Enter your url")
+url= raw_input("Enter your url: ")
 video=pafy.new(url)
-print "Do you Want to Download it or want to extract information ?"
-choice = raw_input("Input d to Download or ei to Extract Information")
+print "Do you Want to Download it or want to extract information?"
+choice = raw_input("Input 'download' to Download or 'extract' to Extract Information: ")
 
-if choice == 'd':
+if choice == 'download':
 	best=video.getbest()
 	best.download(quiet=False)
-elif choice == 'ei':
-	print "Title : " +video.Title
+elif choice == 'extract':
+	print "Title : " +video.title
 	print "Rating : " +str(video.rating) 
-	print "View Count : " + str(video.viewcount) + "Author : " + video.author + "Length :" + sstr(video.length)
+	print "View Count : " + str(video.viewcount) + "Author : " + video.author + "Length :" + str(video.length)
 	print "Duration :" + str(video.duration) + "Likes : "+ str(video.likes) + "Dislikes : "+ str(video.dislikes)
 	print "Description : "+ video.description
 	print "Published on Date : " + str(video.published)
